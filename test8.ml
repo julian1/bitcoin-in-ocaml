2c8f780c29505e762e4386b3ca8985b62e499f81
julian1 <julian.atkinson71@gmail.com>
2015-02-24 21:12:36 +1100

    update

--- test8.ml
+++ test8.ml
@@ -22,36 +22,25 @@ let pos, arg = decs_ s pos len in
 (* use the same byte decoder *)
 
 type t =
-  | Male
+  | Bytes of string
+  | Unknown of int
   | OP_HASH160
   | OP_DUP
   | OP_EQUALVERIFY
-  | OP_CHECKSIG 
-  | Bytes of string
-  | Unknown of int
+  | OP_CHECKSIG
 ;;
 
-(* might just about use the takeN recursive function
-  or just while pos is not at the end...
-  it's not a fold ...
-*)
 
-let ff s =
-  let rec f (pos: int) acc =
+let decode_script s =
+  let rec f pos acc =
     if pos < strlen s then
       let pos, c = decodeInteger8 s pos in
       (* let () = Printf.printf "whoot pos %d\n" pos in *)
-
       if ( c > 0 && c < 76) then
-        
         let len = c in
-        let () = Printf.printf "len %d\n" len in 
-        let pos, arg = decs_ s pos len in 
-       (* let uuu = Unknown 123  in
-        in f pos (uuu::acc)
-    *)  f pos (Bytes arg ::acc)
-
-      else 
+        let pos, arg = decs_ s pos len in
+        f pos (Bytes arg ::acc)
+      else
         let x = match c with
         | 118 -> OP_DUP
         | 169 -> OP_HASH160
@@ -60,29 +49,31 @@ let ff s =
         | _ -> Unknown c
         in f pos (x::acc)
     else pos, acc
-
   in let _, result = f 0 []
   in result
 
 
-let fuck = List.rev ( ff output.pkScript )
+let tokens = List.rev @@ decode_script output.pkScript
 
-let () = Printf.printf "length %d\n" (List.length fuck)
+let () = Printf.printf "length %d\n" (List.length tokens)
 
 let f x =
-  let u = match x with
+  match x with
   | OP_DUP -> "OP_DUP"
   | OP_HASH160 -> "OP_HASH160"
   | OP_EQUALVERIFY-> "OP_EQUALVERIFY"
   | OP_CHECKSIG -> "OP_CHECKSIG"
   | Bytes c -> "Bytes " ^ hex_of_string c
   | Unknown c -> "Unknown " ^ string_of_int c
-  in  
-    Printf.printf " - %s\n" u
 
+(* let u = List.map f tokens *)
 
-(* should use map and string.concat to format the script sig *)
-let () = List.iter f fuck
+(* should use map and string.concat to format the script sig
+  concat with "," as join
+*)
+let () = List.iter (fun x -> Printf.printf "%s " (f x)) tokens
+
+let () = Printf.printf "\n"
 
 
 
