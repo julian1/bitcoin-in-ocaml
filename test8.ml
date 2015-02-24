c9bc73b404c76b0da124e11f064f7ff0181d68f1
julian1 <julian.atkinson71@gmail.com>
2015-02-24 20:33:57 +1100

    update

--- test8.ml
+++ test8.ml
@@ -1,12 +1,12 @@
 
 open Core
-open Message 
+open Message
 
-let get_output () = 
+let get_output () =
   let in_channel = open_in "dumps/0e7b95f5640018b0255d840a7ec673d014c2cb2252641b629038244a6c703ecb" in
   let s = Core.In_channel.input_all in_channel in
   let () = close_in in_channel in
-  let _, tx = decodeTx s 0 in 
+  let _, tx = decodeTx s 0 in
   let () = Printf.printf "* got tx!!!\n%s\n" (formatTx tx) in
   let output_1 :: _ = tx.outputs in
   output_1
@@ -15,21 +15,24 @@ let get_output () =
 let (output : tx_out )  = get_output ()
 
 (*
-let output = get_output () 
-let() = Printf.printf "* length %d\n" len 
+let output = get_output ()
+let() = Printf.printf "* length %d\n" len
 let pos, arg = decs_ s pos len in
 *)
 (* use the same byte decoder *)
 
 type t =
   | Male
+  | OP_HASH160
   | OP_DUP
+  | OP_EQUALVERIFY
+  | OP_CHECKSIG 
   | Unknown of int
 ;;
 
-(* might just about use the takeN recursive function  
+(* might just about use the takeN recursive function
   or just while pos is not at the end...
-  it's not a fold ... 
+  it's not a fold ...
 *)
 
 let ff s =
@@ -37,9 +40,12 @@ let ff s =
 
     if pos < strlen s then
       let pos, c = decodeInteger8 s pos in
-      let () = Printf.printf "whoot pos %d\n" pos in
-      let x = match c with 
-        118 -> OP_DUP 
+      (* let () = Printf.printf "whoot pos %d\n" pos in *)
+      let x = match c with
+        118 -> OP_DUP
+        | 169 -> OP_HASH160
+        | 136 -> OP_EQUALVERIFY
+        | 172 -> OP_CHECKSIG
         | _ -> Unknown c
       in f pos (x::acc)
     else pos, acc
@@ -47,25 +53,17 @@ let ff s =
   in let _, result = f 0 []
   in result
 
-(*  
-  in
-  let _, result = f 0 [] in
-  123 
 
-let s = output.pkScript in
-in
-  let format_ x = match x with 
-    | OP_DUP -> "OP_DUP"
-    | Unknown v -> "Unknown " ^ string_of_int v
-*)
-
-let fuck = ff output.pkScript 
+let fuck = List.rev ( ff output.pkScript )
 
 let () = Printf.printf "length %d\n" (List.length fuck)
 
-let f x = 
-  match x with 
-  | OP_DUP -> Printf.printf "OP_DUP\n" 
+let f x =
+  match x with
+  | OP_DUP -> Printf.printf "OP_DUP\n"
+  | OP_HASH160 -> Printf.printf "OP_HASH160\n"
+  | OP_EQUALVERIFY-> Printf.printf "OP_EQUALVERIFY\n"
+  | OP_CHECKSIG -> Printf.printf "OP_CHECKSIG\n"
   | Unknown c -> Printf.printf "unknown %d\n" c
 
 
