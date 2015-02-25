644bdf11e314945c21093f9f8078bc442e00ba9a
julian1 <julian.atkinson71@gmail.com>
2015-02-25 20:17:14 +1100

    update

--- address.ml
+++ address.ml
@@ -13,22 +13,57 @@
 let s = Z.to_string u in
 Printf.printf "%s\n" s
 
+it would be nice to be able to write this stuff...
+with folds rather than recursive functions...
+
+fold can wo
+- actually it's not a fold over the data it's a fold while a condition is true
+- and it needs the value and acc and boolean
+
 *)
 
-let code_string = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz" in
+let code_string = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz" 
 
 
-let u = Z.of_int 58 in
-let div  = Z.of_string "0xc1a235aafbb6fa1e954a68b872d19611da0c7dc9"  in
+let u = Z.of_int 58 
+let zero = Z.zero
+let div  = Z.of_string "0xc1a235aafbb6fa1e954a68b872d19611da0c7dc9"   
 
-let div, rem = Z.div_rem div u in 
-let rem_ = code_string.[ Z.to_int rem] in
-let () = Printf.printf "whoot %c\n" rem_  in
 
-let div, rem = Z.div_rem div u in 
-let rem_ = code_string.[ Z.to_int rem] in
-Printf.printf "whoot %c\n" rem_ 
+let f div = 
+	let div1, rem = Z.div_rem div u in
+	div1, code_string.[ Z.to_int rem]
 
+let rec ff div acc =
+  if Z.gt div zero then
+    let div, c = f div in
+    ff div (c :: acc) 
+  else
+    acc
+
+
+let result = ff div [] in
+let result2 = Core.Std.String.of_char_list result in
+
+Printf.printf "whoot %s\n" result2
+
+(* rather than constructing a list we should be generating a string *)
+
+
+(*
+Printf.printf "whoot\n"
+*)
+
+
+(*
+
+in
+
+let () = Printf.printf "whoot %c\n" c in
+let div, c = f div in 
+Printf.printf "whoot %c\n" c 
+
+*)
 
 
 
