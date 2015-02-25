7a38e16eb2d7e1d68864b2d5c01e621fbd020d43
julian1 <julian.atkinson71@gmail.com>
2015-02-25 19:33:48 +1100

    update

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
 
+refs
+
+https://en.bitcoin.it/wiki/Protocol_documentation
+https://en.bitcoin.it/wiki/Base58Check_encoding
+https://en.bitcoin.it/wiki/Script
+
+---
 lwt
 opam install zarith
+
+eg,
+
+corebuild  -package zarith,sha,lwt,lwt.unix,lwt.syntax -syntax camlp4o,lwt.syntax address.byte 
 
--- address.ml
+++ address.ml
@@ -1,9 +1,34 @@
 
+(* corebuild  -package zarith,sha,lwt,lwt.unix,lwt.syntax -syntax camlp4o,lwt.syntax address.byte *) 
 
-
-
-let u = Z.of_string "99999999999999999999999"  in
+(*
+   code_string = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz"
+   x = convert_bytes_to_big_integer(hash_result)
+   output_string = ""
+   while(x > 0) 
+       {
+           (x, remainder) = divide(x, 58)
+           output_string.append(code_string[remainder])
+       }
 let s = Z.to_string u in
 Printf.printf "%s\n" s
+
+*)
+
+let code_string = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz" in
+
+
+let u = Z.of_int 58 in
+let div  = Z.of_string "0xc1a235aafbb6fa1e954a68b872d19611da0c7dc9"  in
+
+let div, rem = Z.div_rem div u in 
+let rem_ = code_string.[ Z.to_int rem] in
+let () = Printf.printf "whoot %c\n" rem_  in
+
+let div, rem = Z.div_rem div u in 
+let rem_ = code_string.[ Z.to_int rem] in
+Printf.printf "whoot %c\n" rem_ 
+
+
 
 
