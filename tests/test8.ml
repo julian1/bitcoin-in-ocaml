ffecd7c211c943534831f88b2534d736efadf517
julian1 <julian.atkinson71@gmail.com>
2015-03-13 23:58:43 +1100

    update

--- tests/test8.ml
+++ tests/test8.ml
@@ -6,30 +6,21 @@
 
 open OUnit2
 
-open Lwt (* for >>= *)
-
+let (>>=) = Lwt.bind
 
 let test1 test_ctxt = 
 
-	let () = Lwt_preemptive .init 5 10 (fun err -> Printf.printf "error %s" err  )  in
+	let () = Lwt_preemptive.init 5 10 (fun err -> Printf.printf "error %s" err  )  in
 
 	ignore ( Lwt_main.run (
- 
-
-		Lwt_io.write_line Lwt_io.stdout "whoot1"
-
-		>>= fun _ -> Lwt_preemptive.detach (fun () -> LevelDB.open_db "mydb"   ) ()  
-
-		>>= fun db -> Lwt_preemptive.detach (fun () -> LevelDB.get db "aaa"   ) ()  
-
+		Lwt_io.write_line Lwt_io.stdout "starting"
+		>>  Lwt_preemptive.detach (fun () -> LevelDB.open_db "mydb" ) ()  
+		>>= fun db -> Lwt_preemptive.detach (fun () -> LevelDB.get db "aaa" ) ()  
 		>>= fun v -> Lwt_io.write_line Lwt_io.stdout (match v with
 			| Some s -> s 
 			| None -> "none"
 		)
- 
-		)
-	)  
-
+	))  
 
 
 let tests =
