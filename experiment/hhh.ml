14f07930f9b93358115364fe4d594d79a30c4e3d
julian1 <julian.atkinson71@gmail.com>
2015-03-14 12:41:15 +1100

    update

---
+++ experiment/hhh.ml
@@ -0,0 +1,83 @@
+
+
+let (>>=) = Lwt.bind
+
+let debug = ref false
+let dbg fmt a b = () (* ksprintf (eprintf "Trakeva_sqlite: %s\n%!") fmt *)
+ 
+type t = {
+  handle: Sqlite3.db;
+  action_mutex: Lwt_mutex.t;
+}
+
+
+
+let in_posix_thread ~on_exn f =
+  Lwt_preemptive.detach (fun () ->
+  (* (fun f () -> Lwt.return (f ()))  (fun () -> *)
+    try `Ok (f ())
+    with e -> on_exn e) ()
+
+
+
+let with_executed_statement handle statement f =
+  let prep = Sqlite3.prepare handle statement in
+  (try
+    let x = f prep in
+    let _ = Sqlite3.finalize prep in
+    x
+  with e ->
+    let _ = Sqlite3.finalize prep in
+    raise e)
+
+
+
+let is_ok_or_done_exn handle (rc: Sqlite3.Rc.t) =
+  let open Sqlite3.Rc in
+  match rc with
+  | OK  -> ()
+  | DONE -> ()
+  | _ -> failwith (Printf.sprintf "not ok/done: %s (global error: %s)"
+                     (Sqlite3.Rc.to_string rc)
+                     (Sqlite3.errmsg handle))
+
+
+
+let exec_unit_exn handle statement =
+  with_executed_statement handle statement (fun prep ->
+      Sqlite3.step prep |> is_ok_or_done_exn handle)
+
+
+
+
+
+let load path =
+  let on_exn e = `Error (`Database (`Load path, Printexc.to_string e)) in
+
+	(*
+  begin
+    try if Sys.getenv "TRAKEVA_SQLITE_DEBUG" = "true" then debug := true
+    with _ -> ()
+  end;
+	*)
+  let action_mutex = Lwt_mutex.create () in
+  in_posix_thread ~on_exn (fun () ->
+     (* if !debug then
+        dbg "openning: %S" path; *)
+      (* we need the mutex `FULL: https://www.sqlite.org/threadsafe.html
+         the private cache is up for debate: 
+         https://www.sqlite.org/sharedcache.html *)
+      let handle = Sqlite3.db_open ~mutex:`FULL ~cache:`PRIVATE path in
+      (* exec_unit_exn handle (create_table default_table); *)
+       {handle; action_mutex} 
+    )
+
+
+let _ = Lwt_main.run (
+	load "jjj"
+	(* >>= fun _ -> Lwt_io.write_line Lwt_io.stdout "starting" *)
+	
+	
+
+)	
+
