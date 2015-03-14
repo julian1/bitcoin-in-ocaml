6f6c9a5773b870a1443d63ffb8495a5f317f329d
julian1 <julian.atkinson71@gmail.com>
2015-03-14 18:23:33 +1100

    update

--- tests/test1.ml
+++ tests/test1.ml
@@ -1,15 +1,18 @@
 
-(* decode a block *)
+(* test block header decode *)
 
 open OUnit2
 
 let decodeJ (s:string) pos = 
 	let pos, block = Message.decodeBlock s pos in 
-	let pos, tx = Message.decodeTx s pos in 
-	let pos, tx2 = Message.decodeTx s pos in 
+	let pos, lst = Message.decodeNItems s pos Message.decodeTx block.tx_count in
+ 
+(*	let pos, tx = Message.decodeTx s pos in 
+	let pos, tx2 = Message.decodeTx s pos in  *)
 	let () = Printf.printf "%s\n" ( Message.formatBlock block ) in 
 (*	let () = Printf.printf "tx %s \n" ( Message.formatTx tx )in
 	let () = Printf.printf "tx2 %s \n" ( Message.formatTx tx2 )in *)
+	let () = Printf.printf "lst size %d \n" ( List.length lst  ) in 
 	pos, block 
 
 let test1 test_ctxt =
