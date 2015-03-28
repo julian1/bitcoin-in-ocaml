6183e058cc4381f6127b9f5179fa3e33e821b3cc
julian1 <julian.atkinson71@gmail.com>
2015-03-28 21:26:27 +1100

    update

--- src/client3.ml
+++ src/client3.ml
@@ -687,7 +687,7 @@ Lwt.return block
       let block = Bytes .create len in
       Lwt_unix.read fd block 0 len >>= 
       fun ret ->
-       (* Lwt_io.write_line Lwt_io.stdout @@ "read "  ^ string_of_int ret  *)
+        Lwt_io.write_line Lwt_io.stdout @@ "read bytes - "  ^ string_of_int ret >> 
       return (
         if ret > 0 then Some ( Bytes.to_string block )
         else None 
@@ -707,27 +707,17 @@ Lwt.return block
         | Some s -> 
           let _, header = decodeHeader s 0 in
           Lwt_io.write_line Lwt_io.stdout @@ header.command ^ " " ^ string_of_int header.length  
-
-  
-          >> read_bytes fd header.length 
-          >> loop fd 
-
-(*
-          >> read_bytes fd 200 
+          >> read_bytes fd 84  (* var int tx count is not part of block header *)  
           >>= fun u -> match u with 
             | Some ss -> 
-
-              Lwt_io.write_line Lwt_io.stdout "here" 
-              >> 
-              let _, block_header = decodeBlock ss 0 in
+              Lwt_io.write_line Lwt_io.stdout @@ "here " ^ string_of_int @@ String.length ss 
+              >> let _, block_header = decodeBlock ss 0 in
               Lwt_io.write_line Lwt_io.stdout @@ string_of_int block_header.version 
-(*
-              >> advance fd (header.length - 80 )
-              >> loop fd  *)
-
+              >> advance fd (header.length - 84 )
+              >> loop fd  (* *)
             | None -> 
               return ()
-*)
+
         | None -> 
           return ()
     in 
