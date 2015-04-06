58cbdaac74e3f8c0035924452ee7a5ed160fa7c1
julian1 <julian.atkinson71@gmail.com>
2015-04-06 12:35:12 +1000

    update

--- src/client5.ml
+++ src/client5.ml
@@ -6,7 +6,7 @@
   - we should wrap the actions to be simpler... 
 
   - we can easily make a fold type function with the iterator.
-  - mostly we want to store the lseek pos
+  - mostly we want to store the lseek pos into the blocks 
 *)
 module M = Message
 
@@ -18,10 +18,23 @@ let detach f =
     (fun () -> f ) () 
 
 
-module SSS = 
+module DB = 
   struct
+	  let open_db n = detach @@ LevelDB.open_db n 
+
     let get_key i = detach @@ LevelDB.Iterator.get_key i
     let get_value i = detach @@ LevelDB.Iterator.get_value i
+
+    let get_keyval i = detach @@ (
+      let key = LevelDB.Iterator.get_key i  in
+      let value = LevelDB.Iterator.get_value i in 
+      (key, value)
+    )
+
+	  let make db = detach @@ LevelDB.Iterator.make db 
+	  let seek_to_first i = detach @@ LevelDB.Iterator.seek_to_first i 
+		let next i = detach @@ LevelDB.Iterator.next i 
+    let valid i = detach @@ LevelDB.Iterator.valid i
   end 
 
 
@@ -38,30 +51,28 @@ let run () =
 
 	let rec loop i = 
 		(* need to decode the index *)
-		SSS.get_key i 
+    DB.get_keyval i >>= fun (key,value) -> 
+(* 
+		DB.get_key i 
 		>>= fun key -> 
-		SSS.get_value i 
+		DB.get_value i 
 		>>= fun value -> 
+*)
       let pos = 0 in
       let pos, hash = M.decodeHash32 key pos in 
       let pos, index = M.decodeInteger32 key pos in 
 
 			write_stdout @@ M.hex_of_string hash ^ " " ^ string_of_int index ^ " " ^ value
-		>> detach @@ LevelDB.Iterator.next i 
 
-    >> if LevelDB.Iterator.valid i then  
+		>> DB.next i 
+
+    >> DB.valid i >>= fun valid -> if valid then  
       loop i 
     else
       return ()
-
-
-
 	in
-	detach @@ LevelDB.open_db "mydb" 
-	>>= fun db -> 
-	detach @@ LevelDB.Iterator.make db 
-	>>= fun i -> 
-	detach @@ LevelDB.Iterator.seek_to_first i 
+	DB.open_db "mydb" >>= fun db -> 
+	DB.make db >>= fun i -> DB.seek_to_first i 
 	>> 
 	loop i
 )
