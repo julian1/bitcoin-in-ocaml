ba74cac6fdc28796bd0232f40ae20757187bf08a
julian1 <julian.atkinson71@gmail.com>
2015-04-06 19:34:05 +1000

    update

--- src/index.ml
+++ src/index.ml
@@ -15,6 +15,18 @@ type key =
 
 
 
+
+let encodeKey (h : key ) =
+  M.encodeHash32 h.hash 
+  ^ M.encodeInteger32 h.index
+
+let decodeKey s  =
+  let pos, hash = M.decodeHash32 s 0 in
+  let _, index = M.decodeInteger32 s pos in 
+  { hash = hash; index = index } 
+
+
+
 let decodeValue s =
   let pos, status = M.decodeString s 0 in 
   let pos, lseek =  M.decodeInteger64 s pos in 
@@ -23,17 +35,5 @@ let decodeValue s =
 let encodeValue (h : value)  =
   M.encodeString h.status
   ^ M.encodeInteger64 (Int64.of_int h.lseek ) 
-
-
-let encodeKey (h : key ) =
-  M.encodeHash32 h.hash 
-  ^ M.encodeInteger32 h.index
-
-let decodeKey s  =
-  let pos, hash = M.decodeHash32 s 0 in
-  let _, index = M.decodeInteger32 s, pos in 
-  { hash = hash; index = index } 
-
-
 
 
