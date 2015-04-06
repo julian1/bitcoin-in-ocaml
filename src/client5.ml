ff41eb2c7af47e02a52d558bc0ac48e59814b98a
julian1 <julian.atkinson71@gmail.com>
2015-04-06 19:15:02 +1000

    update

--- src/client5.ml
+++ src/client5.ml
@@ -19,7 +19,7 @@ let return = Lwt.return
 let write_stdout = Lwt_io.write_line Lwt_io.stdout 
 
 let run () = 
-	Lwt_main.run 
+	Lwt_main.run ( 
     let rec loop i = 
       Db.get_keyval i >>= fun (key,value) -> 
         let pos, hash = M.decodeHash32 key 0 in 
@@ -36,6 +36,7 @@ let run () =
     Db.make db >>= fun i -> Db.seek_to_first i 
     >> 
     loop i
+  )
 
 let () = run ()
 
