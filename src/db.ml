0313d3eb756d834a8c705516ac3ba5d070dc4f66
julian1 <julian.atkinson71@gmail.com>
2015-05-31 22:18:05 +1000

    update

--- src/db.ml
+++ src/db.ml
@@ -15,10 +15,9 @@ let detach f =
 let open_db n = detach @@ LevelDB.open_db n 
 
 let get_key i = detach @@ LevelDB.Iterator.get_key i
-(* 
 let get_value i = detach @@ LevelDB.Iterator.get_value i
-*)
 
+(* shouldn't really use this *)
 let get_keyval i = detach @@ (
 	let key = LevelDB.Iterator.get_key i  in
 	let value = LevelDB.Iterator.get_value i in 
