edd94cec58f9ba6f7d0ecf8f9f27393d146d3477
julian1 <julian.atkinson71@gmail.com>
2015-06-25 21:57:22 +1000

    update

--- test01b.sql
+++ test01b.sql
@@ -1,20 +1,21 @@
 
 -- always a join b on 
 
-explain select * --, x.received, x.unspent
-from test 
-t 
-join 
-    (input 
-    join (output 
-          join ( output_address 
-                join address on address.id = output_address.address_id )
-
-
-          on output_address.output_id = output.id)
+select * --, x.received, x.unspent
+from 
+(test join 
+    (input join 
+        (output join 
+            (output_address join 
+                address  
+            on address.id = output_address.address_id )
+        on output_address.output_id = output.id)
     on output.id = input.output_id )
-on input.id = t.input_id
+on input.id = test.input_id)
 ;
+
+
+-- join x on x.hash = address.hash )
 
 -- join output_address oa on oa.output_id = o.id
 
