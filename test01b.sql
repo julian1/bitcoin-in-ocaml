8519035125c39aa2d875d199256c463d9661173e
julian1 <julian.atkinson71@gmail.com>
2015-06-25 21:48:35 +1000

    update

--- test01b.sql
+++ test01b.sql
@@ -6,7 +6,12 @@ from test
 t 
 join 
     (input 
-    join (output join output_address on output_address.output_id = output.id)
+    join (output 
+          join ( output_address 
+                join address on address.id = output_address.address_id )
+
+
+          on output_address.output_id = output.id)
     on output.id = input.output_id )
 on input.id = t.input_id
 ;
