19863cc0f59e0b70c8faeace184d309337942f22
julian1 <julian.atkinson71@gmail.com>
2015-06-28 21:22:36 +1000

    update

--- test02.sql
+++ test02.sql
@@ -15,6 +15,22 @@ select * from
     -- ROW_NUMBER() OVER(PARTITION BY r order by id ) AS Row
     FROM signature ) as s2
 
+    -- actually want >= so can see last date of change.
     where Row =  2
 ;
+
+
+select 
+    t.r,  
+    a.hash, 
+    format_amount( received( a.hash) ) as received, 
+    format_amount( unspent( a.hash) ) as unspent 
+from test t 
+join input i on i.id = t.input_id
+join output o on o.id = i.output_id
+join output_address oa on oa.output_id = o.id
+
+join address a on a.id = oa.address_id 
+
+
 
