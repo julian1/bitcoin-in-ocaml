30deb5c0cefa9159e8ec63a29f7d2e6d41aa5f25
julian1 <julian.atkinson71@gmail.com>
2015-07-09 08:37:47 +1000

    update

--- src/client.ml
+++ src/client.ml
@@ -85,14 +85,16 @@ let run () =
 
               let state = { state with jobs = incomplete } in
 
-              (* add events to the queue *) 
-              let f queue e = 
+              (* process io events *) 
+              let f (state,queue) e = 
                 match e with 
-                  | U.Nop -> queue
-                  | SeqJobFinished -> queue
-                  | _ -> Myqueue.add queue e  
+                  | U.Nop -> state,queue
+                    (* a seq job finished then take the new state *) 
+                  | SeqJobFinished -> state,queue
+                    (* any other event gets added to queue *)
+                  | _ -> state, Myqueue.add queue e 
               in
-              let queue = List.fold_left f queue complete in
+              let state,queue = List.fold_left f (state,queue) complete in
 
               log @@ " here !! jobs " 
                 ^ (string_of_int (List.length state.jobs)) 
@@ -100,15 +102,59 @@ let run () =
                 ^ (string_of_int (Myqueue.length queue )) 
 
               >>   
+              (* why are we passing state around everywhere 
+                when a seq job finishes it has to be able to return the new state...
+
+                - we need to hand off the state to the sequence job...
+                - not pass it around here...
+              *)  
+
+              (* take one event off the queue, only if one isn't already running ? *)
               let state,queue = 
-              if queue <> Myqueue.empty then 
-                let job,queue = Myqueue.take queue in
- 
- 
-                (* take one job off the queue, only if one isn't already running ? *)
-                state,queue 
-              else
-                state,queue 
+                if queue <> Myqueue.empty then 
+                  let e,queue = Myqueue.take queue in
+  
+                  (* we inject the state into the job which owns it for this action... 
+                        there's a problem...
+                        the jobs type returns event types so we can't define it... 
+                        -----
+                        why bother adding the thing jobs? because that's what we check to 
+                        see what has advanced...
+                        ----
+                        we don't have to pass state in loop. instead just push a queue job
+                        to return it...
+                        --- 
+                        VERY IMPORTANT
+                        - can we structure the code so all the complex state manipulation
+                        only happens in response to a seqJObFinished. which might mean
+                        we won't have to swing around the state. except in the que
+                        -----
+                        ugghhh. there's still a whole ordering issue here.
+                        -----
+                        OK. rather than forcing everything through the single nchoose picker
+                        ---- 
+
+                        - have two io subsystems. jobs and sequence jobs
+                            - choose completed state.jobs and push stuff onto queue... 
+                            - process the queue and update state.
+                        problem is using same queue in both. we cant thread/share 
+                  *) 
+
+                  let state = 
+                    { state with
+                      seq_job_running = true;
+                      jobs = (
+                        (* state is injected into the seq job *)
+                        let state = P2p.update state e
+                        in return Misc.SeqJobFinished
+                      )
+                        :: state.jobs;
+                    }
+                  in
+
+                  state,queue 
+                else
+                  state,queue 
               in
 
             
