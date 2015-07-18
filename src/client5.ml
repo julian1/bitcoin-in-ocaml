4921a44845557894edd8700f162d1a283ad37076
julian1 <julian.atkinson71@gmail.com>
2015-07-18 22:45:49 +1000

    wif encoding

--- src/client5.ml
+++ src/client5.ml
@@ -7,14 +7,25 @@ module A = Address
 
 (*let s =  (char_of_int 0x80) ^ s  in *)
 
-let wif_encode s =
+(*
+  probably want a decode as well, so we can work with them directly
+  - ughh we need an base58 unencode function
+  
+  TODO maybe change name to wif_of_string ?
+*)
+
+let wif_encode s compressed =
   let s = M.string_of_hex "80" ^ s in  
+  let s = match compressed with
+    | true -> s ^ M.string_of_hex "01"
+    | false -> s
+  in
   let checksum = M.sha256d s |> (fun s -> S.sub s 0 4) in
   let s = s ^ checksum in
-  let s = A.base58_of_string s in 
-  s 
+  A.base58_of_string s
 
-let s =  wif_encode (M.string_of_hex "0c28fca386c7a227600b2fe50b7cae11ec86d3bf1fbe471be89827e19d72aa1d") in
+
+let s =  wif_encode (M.string_of_hex "0c28fca386c7a227600b2fe50b7cae11ec86d3bf1fbe471be89827e19d72aa1d") false in
 print_endline  s 
 
 
