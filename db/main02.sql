0765962095f047f00c4c8064b1bac8bca46acc24
julian1 <julian.atkinson71@gmail.com>
2015-07-18 17:09:41 +1000

    update

--- db/main02.sql
+++ db/main02.sql
@@ -7,12 +7,14 @@ begin;
 
 -- can use materialized views for height etc if . may not be bad...
 ------------
+drop view if exists  _leaves2; 
 drop view if exists  _leaves; 
+drop view if exists  _height; 
 
 create view _leaves as
 select
-  b.id as bid,
-  b.hash as b
+  b.id as id,
+  b.hash as hash
   -- p.*
 from block b
 left join previous p on p.block_previous_id = b.id 
@@ -20,7 +22,7 @@ where p.id is null
 ;
 
 
-create or replace view _height as
+create view _height as
 with recursive t( id, height ) AS (
   select (
     -- tree root
@@ -28,7 +30,7 @@ with recursive t( id, height ) AS (
     from block 
     left join previous on previous.block_id = block.id 
     where previous.id is null
-    ), 0
+  ), 0
   UNION ALL
   SELECT 
     block_id, t.height + 1
@@ -39,6 +41,20 @@ with recursive t( id, height ) AS (
 select t.height, block.* 
 FROM t join block on block.id = t.id;
 
+
+
+create view _leaves2 as
+select 
+  now() - time as when, 
+  time, 
+  _height.height,
+  _leaves.hash,
+  _leaves.id as block_id
+from _leaves
+join _height on _height.id = _leaves.id 
+--order by height desc
+
+;
 
 
 commit;
