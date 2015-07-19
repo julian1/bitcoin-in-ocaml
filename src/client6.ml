92ffadff14259e3f936ca1dd4e01223f4acf2c29
julian1 <julian.atkinson71@gmail.com>
2015-07-19 12:42:03 +1000

    update

--- src/client6.ml
+++ src/client6.ml
@@ -6,8 +6,7 @@ module CS = Core.Core_string
 
 (* think we have to use z, noting that there's a big ugly division in there... 
 
-Z.of_int: int -> t
-0x0404cb * 2**(8*(0x1b - 3)) = 0x00000000000404CB000000000000000000000000000000000000000000000000
+  https://en.bitcoin.it/wiki/Difficulty
 *)
 
 
@@ -19,27 +18,26 @@ let string_of_z z = z |> Z.to_bits |> CS.rev
 let z_of_string s = s |> CS.rev |> Z.of_bits
 
 
-let target_1_difficulty = 
-  "00000000FFFF0000000000000000000000000000000000000000000000000000" 
-  |> M.string_of_hex |> z_of_string 
 
-
-(* change name to decode difficulty *)
-let difficulty_target s =
+let decode_difficulty s =
   let exp = get s 0 in 
   let base = (get s 3) + (get s 2 lsl 8) + (get s 1 lsl 16) in 
   Z.mul (Z.of_int base) (Z.pow (Z.of_int 2) (8 * (exp - 3))) 
 
 
 let bdiff v = 
-  (* todo implement the fractional component *)
+  let target_1_difficulty = 
+    "00000000FFFF0000000000000000000000000000000000000000000000000000" 
+    |> M.string_of_hex |> z_of_string 
+  in
+  (* TODO implement fractional component *)
   let bdiff = Z.div target_1_difficulty v  in
   Z.to_float bdiff 
 
 
 
 let s = M.string_of_hex "1b0404cb" in 
-let d = difficulty_target s in
+let d = decode_difficulty s in
 let bdiff = bdiff d in
 
 (* how to we get the full float value *)
