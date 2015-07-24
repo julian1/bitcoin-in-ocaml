fb39d1bc3ad5b4aa671054dfba95e9cdd1292662
julian1 <julian.atkinson71@gmail.com>
2015-07-24 16:15:24 +1000

    update

--- db/test01.sql
+++ db/test01.sql
@@ -2,18 +2,21 @@
 drop view if exists _main ;
 
 create view _main as
-with recursive t( id ) AS (
+with recursive t( id, depth ) AS (
+  select (
+    -- tree root
     select block.id 
     from block 
     where hash = '\x0000000000000000147011a429bd8c2e30475ad61eaa544c3b971e99bb7bc307'  -- chain tip
+  ), 0
   UNION ALL
   SELECT 
-    block_previous_id
+    block_previous_id, t.depth + 1
   FROM block
   join previous on previous.block_previous_id = block.id 
   join t on t.id = previous.block_id
 )
-select block.* 
+select t.depth, block.* 
 FROM t join block on block.id = t.id;
 
 
