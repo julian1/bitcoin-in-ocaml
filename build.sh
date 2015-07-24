d4594b08081f0b6e1c2e15e6132bf74c99bc2401
julian1 <julian.atkinson71@gmail.com>
2015-07-24 15:18:43 +1000

    update

--- build.sh
+++ build.sh
@@ -2,5 +2,9 @@
 
 # rm -rf ./_build 
 
-corebuild -I src  -package pgocaml,cryptokit,zarith,lwt,lwt.preemptive,lwt.unix,lwt.syntax  test/runner.byte
+corebuild -no-links -I src  -package pgocaml,cryptokit,zarith,lwt,lwt.preemptive,lwt.unix,lwt.syntax  test/runner.byte
+
+# ./_build/test/runner.byte
+
+corebuild  -no-links  -I src  -package microecc,pgocaml,cryptokit,zarith,lwt,lwt.preemptive,lwt.unix,lwt.syntax  src/client.byte src/client2.byte src/client3.byte src/client4.byte
 
