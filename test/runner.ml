868e1753e069629f083618465c7ef3bceb407c5e
julian1 <julian.atkinson71@gmail.com>
2015-07-24 15:35:05 +1000

    tidy

--- test/runner.ml
+++ test/runner.ml
@@ -5,26 +5,18 @@ open OUnit2;;
   test composition functions are ok
   http://ounit.forge.ocamlcore.org/api-ounit-dev/OUnit2.html 
 
-  val (>:) : string -> test -> test 
-  Create a TestLabel for a test
-
-  val (>::) : string -> test_fun -> test
-  Create a TestLabel for a TestCase
-
-  val (>:::) : string -> test list -> test
-  Create a TestLabel for a TestList
-
-  val test_list : test list -> test
-  Generic function to create a test list.
+  val (>:) : string -> test -> test  - Create a TestLabel for a test
+  val (>::) : string -> test_fun -> test   - Create a TestLabel for a TestCase
+  val (>:::) : string -> test list -> test  - Create a TestLabel for a TestList
+  val test_list : test list -> test  - Generic function to create a test list.
 *)
 
 let () =
-  run_test_tt_main (
-    test_list [ 
+  run_test_tt_main 
+    ( test_list [ 
       "label" >: Address_test.tests; 
       Message_test.tests; 
       Wif_test.tests; 
       Difficulty_test.tests; 
-    ]
-  )  
+    ])  
 
