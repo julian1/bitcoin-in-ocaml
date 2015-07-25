a058539f0be0db24704979374938ed48b5cb7810
julian1 <julian.atkinson71@gmail.com>
2015-07-25 16:03:24 +1000

    update

--- test/merkle_test.ml
+++ test/merkle_test.ml
@@ -25,41 +25,50 @@ let compare a b =
 
 
 (* fold isn't right - unless we do a modulo or something - it has to skip over the list two at a time 
-
   we should be able to gobble it up recursively with patterns
-
   ok, we can fold
 *)
 
 
+(* ok, if we're foldding then we have to keep the order
+
+  does fold_left preserve order ?
+ *)
+
 let hash tx = tx |> M.sha256d |> M.strrev 
 
 let rec f lst =
+
+  let () = print_endline @@ "count " ^ (string_of_int <| L.length) lst in
   match lst with 
     | e :: [] -> e 
     | lst ->
-      let hash tx = tx |> M.sha256d |> M.strrev in
       let aggregate (lst,previous) e = match previous with
         | None -> lst, Some e
         | Some e2 -> hash (e2 ^ e) :: lst, None 
       in
+      (* there's something going wrong with the last element *)
       let lst, previous = L.fold_left aggregate ([],None) lst in
-      f lst 
+      match previous with 
+        | None -> f lst 
+        | Some e -> f ( lst @ [ hash e ^ e ] )
 
 
 let test1 ctx =
   M.(
   let s = read_file "test/data/000000000000000007c5b3e47c690e6ab9e75fdf1f47bfc7a247f29176be6d9f" in 
-  (* let _, header = M.decodeBlock s 0 in *)
+   let _, header = M.decodeBlock s 0 in 
   let txs = M.decode_block_txs s in 
   let () = print_endline @@ "count " ^ (string_of_int <| L.length) txs in
+  let () = print_endline @@ "merkle " ^ M.hex_of_string header.merkle in
   let hash_of_tx tx = S.sub s tx.pos tx.length |> hash in 
   let txs = L.map hash_of_tx txs in
   let txs = L.sort compare txs in  
 
-  let _ = L.fold_left (fun acc hash -> print_endline @@ M.hex_of_string hash) () txs in
+(*  let _ = L.fold_left (fun acc hash -> print_endline @@ M.hex_of_string hash) () txs in *)
 
-  let _ = f txs in
+  let ret = f txs in
+  let () = print_endline @@ "ret " ^ M.hex_of_string ret in
  
   assert_bool "true" true  
   )
