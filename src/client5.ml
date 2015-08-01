1daa6ea920ba118e704f5af3575b3753726e947c
julian1 <julian.atkinson71@gmail.com>
2015-08-01 14:42:32 +1000

    update

--- src/client5.ml
+++ src/client5.ml
@@ -5,6 +5,7 @@ let return = Lwt.return
 module M = Message
 module U = Util
 module L = List
+module S = String
 
 
 type whoot_t =
@@ -19,22 +20,29 @@ let log s = U.write_stdout s
 select block_id from previous where block_previous_id = 2
 *)
 
+let check_block hash data =
+  return () 
+
+
 let rec loop whoot =
   log "here"
   >> U.PG.prepare whoot.db ~query:"select hash,data from block left join block_data on block_data.block_id = block.id where block.id = $1" ()
   >> U.PG.execute whoot.db ~params:[ Some (U.PG.string_of_int whoot.block_id ) ] ()
   >>= function
-      | (Some field ::_ )::_ -> 
-          let hash = U.PG.bytea_of_string field in
-          log @@ "hash " ^ M.hex_of_string hash
-
+      | (Some hash :: Some data :: _ )::_ -> 
+          let hash = U.PG.bytea_of_string hash in
+          let data = U.PG.bytea_of_string data in
+          log @@ "hash " ^ M.hex_of_string hash ^ " len " ^ string_of_int @@ S.length data 
+          >> check_block hash data
+         
+          (* lookup the next entry *)  
           >> U.PG.prepare whoot.db ~query:"select block_id from previous where block_previous_id = $1" ()
           >> U.PG.execute whoot.db ~params:[ Some (U.PG.string_of_int whoot.block_id ) ] ()
           >>= function
               | (Some field::_ )::_ -> 
               let block_id = U.PG.int_of_string field in
               log @@ "next block_id " ^ string_of_int block_id 
-          >> return ()
+              >> loop { whoot with block_id = block_id } 
 
       | _ -> raise (Failure "couldn't get hash")
 
@@ -51,7 +59,7 @@ let start () =
   >>
     let state = {
       db = db;
-      block_id = 2;
+      block_id = 3;
     }
     in
       loop state
