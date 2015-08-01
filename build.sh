1a1a16011250ba1dafe8aa715658a1571e16644a
julian1 <julian.atkinson71@gmail.com>
2015-08-01 13:28:52 +1000

    build

--- build.sh
+++ build.sh
@@ -6,6 +6,9 @@
 
 # ./_build/test/runner.byte
 
+# to generate annot file to check tail-recursion 
+# corebuild -cflag -annot -no-links  -I src  \
+
 corebuild  -no-links  -I src  \
   -package microecc,pgocaml,cryptokit,zarith,lwt,lwt.preemptive,lwt.unix,lwt.syntax  \
  test/runner.byte  src/client.byte src/client2.byte src/client3.byte src/client4.byte
