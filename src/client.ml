b63bf931d50621481c733032c1f469203a4e47aa
julian1 <julian.atkinson71@gmail.com>
2015-08-01 12:53:20 +1000

    update

--- src/client.ml
+++ src/client.ml
@@ -30,76 +30,76 @@ let log s = U.write_stdout s
 
 
 let rec loop (whoot : whoot_t ) =
-    (* select completed jobs *)            
-    Lwt.nchoose_split whoot.jobs
-    >>= fun (complete, incomplete) ->
+  (* select completed jobs *)            
+  Lwt.nchoose_split whoot.jobs
+  >>= fun (complete, incomplete) ->
 
-      (* update incoplete jobs *)
-      let whoot = { whoot with jobs = incomplete } in 
+    (* update incoplete jobs *)
+    let whoot = { whoot with jobs = incomplete } in 
 
-      (* process complete io jobs *) 
-      let f whoot e = 
-        match e with 
-          (* nop *)
-          | U.Nop -> whoot 
-          (* a seq job finished, so take take the new state and add any new jobs*) 
-          | SeqJobFinished (newstate, newjobs) -> { 
-            whoot with state = Some newstate; 
-            jobs =  newjobs @ whoot.jobs ;
-          } 
-          (* any other normal event gets added to queue for processing *)
-          | _ -> { whoot with queue = Myqueue.add whoot.queue e }  
-      in
-      let whoot = L.fold_left f whoot complete in
+    (* process complete io jobs *) 
+    let f whoot e = 
+      match e with 
+        (* nop *)
+        | U.Nop -> whoot 
+        (* a seq job finished, so take take the new state and add any new jobs*) 
+        | SeqJobFinished (newstate, newjobs) -> { 
+          whoot with state = Some newstate; 
+          jobs =  newjobs @ whoot.jobs ;
+        } 
+        (* any other normal event gets added to queue for processing *)
+        | _ -> { whoot with queue = Myqueue.add whoot.queue e }  
+    in
+    let whoot = L.fold_left f whoot complete in
 
-      log @@ " here !! jobs " ^ (string_of_int (L.length whoot.jobs)) 
-        ^ " queue " ^ (string_of_int (Myqueue.length whoot.queue )) 
-        ^ " conns " ^ (match whoot.state with 
-            | Some state -> (string_of_int (L.length state.connections )) 
-            | None -> "unknown"  
-        )
-    >>   
-      (* process a queue item if we can *)
-      let whoot = 
-        if whoot.queue <> Myqueue.empty && whoot.state <> None then 
-          let e,queue = Myqueue.take whoot.queue in
-          {  (* whoot with *) 
-            queue = queue;
-            jobs = (
-              (* state is injected into the seq job, and nulled *)
-              let Some state = whoot.state in
-                P2p.update state e 
-             >>= fun (U.SeqJobFinished (state, jobs1)) ->
-                Chain.update state e 
-             >>= fun (U.SeqJobFinished (state, jobs2)) ->
-              return ( U.SeqJobFinished (state,jobs1 @ jobs2))
+    log @@ " here !! jobs " ^ (string_of_int (L.length whoot.jobs)) 
+      ^ " queue " ^ (string_of_int (Myqueue.length whoot.queue )) 
+      ^ " conns " ^ (match whoot.state with 
+          | Some state -> (string_of_int (L.length state.connections )) 
+          | None -> "unknown"  
+      )
+  >>   
+    (* process a queue item if we can *)
+    let whoot = 
+      if whoot.queue <> Myqueue.empty && whoot.state <> None then 
+        let e,queue = Myqueue.take whoot.queue in
+        {  (* whoot with *) 
+          queue = queue;
+          jobs = (
+            (* state is injected into the seq job, and nulled *)
+            let Some state = whoot.state in
+              P2p.update state e 
+           >>= fun (U.SeqJobFinished (state, jobs1)) ->
+              Chain.update state e 
+           >>= fun (U.SeqJobFinished (state, jobs2)) ->
+            return ( U.SeqJobFinished (state,jobs1 @ jobs2))
 
-            ) :: whoot.jobs;
-            state = None;
-          }
-        else
-          whoot
-      in
-   
-      (* more jobs to process? *) 
-      if L.length whoot.jobs > 0 then
-        loop whoot 
+          ) :: whoot.jobs;
+          state = None;
+        }
       else
-        log "finishing - no more jobs to run!!"
-        >> return ()
+        whoot
+    in
+ 
+    (* more jobs to process? *) 
+    if L.length whoot.jobs > 0 then
+      loop whoot 
+    else
+      log "finishing - no more jobs to run!!"
+      >> return ()
 
 
 let loop' whoot =
   Lwt.catch (
     fun () -> loop whoot 
   )
-    (fun exn ->
-      (* must close *)
-      let s = Printexc.to_string exn  ^ "\n" ^ (Printexc.get_backtrace () ) in
-      log ("finishing - exception " ^ s )
-      >> (* just exist cleanly *)
-        return ()
-    )
+  (fun exn ->
+    (* must close *)
+    let s = Printexc.to_string exn  ^ "\n" ^ (Printexc.get_backtrace () ) in
+    log ("finishing - exception " ^ s )
+    >> (* just exist cleanly *)
+      return ()
+  )
 
 
 let () =
@@ -111,23 +111,22 @@ let () =
     >>= fun db ->
       Processblock.create_prepared_stmts db 
     >>
-        (* we actually need to read it as well... as write it... *)
-
-        let whoot = {
-          state = Some ({
-            network = Dogecoin;
-            connections = [];
-            db = db; 
-            (* should be hidden ?? *)
-            block_inv_pending  = None;
-            blocks_on_request = U.SS.empty;
-            last_block_received_time = [];
-          } : U.my_app_state )
-          ; 
-          jobs = P2p.create(); 
-          queue = Myqueue.empty ;
-        }  
-        in
+      (* we actually need to read it as well... as write it... *)
+      let whoot = {
+        state = Some ({
+          network = Dogecoin;
+          connections = [];
+          db = db; 
+          (* should be hidden ?? *)
+          block_inv_pending  = None;
+          blocks_on_request = U.SS.empty;
+          last_block_received_time = [];
+        } : U.my_app_state )
+        ; 
+        jobs = P2p.create(); 
+        queue = Myqueue.empty ;
+      }  
+      in
           loop' whoot 
   )
 
