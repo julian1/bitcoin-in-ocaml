e97effe1696e6e8d2a36e8fc41c39d0692a5b5a7
julian1 <julian.atkinson71@gmail.com>
2015-08-03 21:46:34 +1000

    update

--- db/test07.sql
+++ db/test07.sql
@@ -6,21 +6,26 @@ drop view locator_hashes;
 
 create view locator_hashes as
 
+(
 with recursive t( i, start_, step ) AS (
-    -- tree root
-    select 100, 1, 1
+  -- tree root
+  -- we can presumably insert the first value...
+  select 100, 1, 1
   UNION ALL
   SELECT 
-      t.i - t.step, t.start_ + 1, 
-      CASE
-      WHEN t.start_ >= 10 THEN t.step * 2 
+      t.i - t.step, 
+      t.start_ + 1, 
+      CASE WHEN t.start_ >= 10 THEN t.step * 2 
       ELSE t.step
   END  
   FROM t 
   where t.i > 0 
 )
-
-select * 
-FROM t; 
+select i 
+FROM t where t.i > 0
+)
+union all 
+select 0
+;
 
 commit;
