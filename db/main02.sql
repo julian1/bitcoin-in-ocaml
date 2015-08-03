3dc594d360efcf612745ad098cb14eadc1dc559b
julian1 <julian.atkinson71@gmail.com>
2015-08-03 22:22:35 +1000

    very good tidying of main views

--- db/main02.sql
+++ db/main02.sql
@@ -7,64 +7,73 @@ begin;
 
 -- can use materialized views for height etc if . may not be bad...
 ------------
+drop view if exists  _leaves2_dynamic; 
+
 drop view if exists  _leaves2; 
 drop view if exists  _leaves; 
 drop view if exists  _height; 
 
+drop view if exists _height_dynamic ;
+drop view if exists _genesis ;
+
 create view _leaves as
-select
-  b.id as block_id,
-  b.hash as hash
-  -- p.*
-from block b
-left join previous p on p.block_previous_id = b.id 
-where p.id is null
+  select
+    b.id as block_id
+  from block b
+  left join previous p on p.block_previous_id = b.id 
+  where p.id is null
 ;
 
+create view _genesis as
+  select 
+    block.id as block_id
+  from block 
+  left join previous on previous.block_id = block.id 
+  where previous.id is null
+;
 
 create view _height_dynamic as
-with recursive t( id, height_ ) AS (
-  select (
-    -- tree root
-    select block.id 
-    from block 
-    left join previous on previous.block_id = block.id 
-    where previous.id is null
-  ), 0
-  UNION ALL
-  SELECT 
-    block_id, t.height_ + 1
-  FROM block
-  join previous on previous.block_id = block.id 
-  join t on t.id = previous.block_previous_id
-)
-select t.height_, block.* 
-FROM t join block on block.id = t.id;
-
+  with recursive t( id, height_ ) AS (
+    select (
+      -- genesis - only selects one of them...
+      select block_id from _genesis 
+    ), 0
+    UNION ALL
+    SELECT 
+      block_id, t.height_ + 1
+    FROM block
+    join previous on previous.block_id = block.id 
+    join t on t.id = previous.block_previous_id
+  )
+  select t.height_, block.* 
+  FROM t 
+  join block on block.id = t.id
+;
 
 
 create view _leaves2_dynamic as
-select 
-  now() - time as when, 
-  time, 
-  _height_dynamic.height,
-  _leaves.hash,
-  _leaves.block_id 
-from _leaves
-join _height_dynamic on _height_dynamic.id = _leaves.block_id 
--- order by _height.height desc
+  select 
+    now() - block.time as when, 
+    block.time, 
+    _height_dynamic.height,
+    block.hash,
+    _leaves.block_id 
+  from _leaves
+  join _height_dynamic on _height_dynamic.id = _leaves.block_id 
+  join block on block.id = _leaves.block_id
+  -- order by _height.height desc
 ;
 
 create view _leaves2 as
-select 
-  now() - block.time as when, 
-  block.time, 
-  block.height,
-  block.hash,
-  block.id as block_id
-from _leaves
-join block on block.id = _leaves.block_id 
--- order by _height.height desc
+  select 
+    now() - block.time as when, 
+    block.time, 
+    block.height,
+    block.hash,
+    block.id as block_id
+  from _leaves
+  join block on block.id = _leaves.block_id 
+  -- order by _height.height desc
 ;
 
 commit;
